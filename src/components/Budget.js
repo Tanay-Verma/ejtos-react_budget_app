019305d3d2903ebc73d10915d3ca2fdd955a591f
Tanay <tanayverma98@gmail.com>
2023-07-25 17:57:49 +0530

    Task 2 Completed

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,23 +1,38 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext, useState, useEffect } from "react";
+import { AppContext } from "../context/AppContext";
 const Budget = () => {
-    
-    const { budget, dispatch } = useContext(AppContext);
-    const [b, setB] = useState(budget);
-    
-    useEffect(() => {
-        dispatch({
-            type: 'SET_BUDGET',
-            payload: b
-        })
-    }, [b,dispatch]);
+  const { budget, dispatch } = useContext(AppContext);
+  const [b, setB] = useState(budget);
 
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £<input type='number'  step='10' max='20000' onChange={(event) => {
-            setB(oldB => event.target.value);
-            }} value={b} /></span>
-        </div>
-    );
+  useEffect(() => {
+    dispatch({
+      type: "SET_BUDGET",
+      payload: b,
+    });
+  }, [b, dispatch]);
+
+  return (
+    <div className="alert alert-secondary">
+      <span>
+        Budget: £
+        <input
+          type="number"
+          step="10"
+          max="20000"
+          onChange={(event) => {
+            setB(() => {
+              if (event.target.value > 20000) {
+                alert("Budget Value cannot exceed 20000");
+                return 20000;
+              } else {
+                return event.target.value;
+              }
+            });
+          }}
+          value={b}
+        />
+      </span>
+    </div>
+  );
 };
 export default Budget;
